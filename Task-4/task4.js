ab380a8eb0566d694036432759be6c9cc7e2cef4
arumugam.s <96759953+Arumugaminfotech@users.noreply.github.com>
2022-04-07 10:32:38 +0530

    Update task4.js

--- Task-4/task4.js
+++ Task-4/task4.js
@@ -40,42 +40,6 @@
 
 
     }
-    
-
-    function createtablerow() {
-
-        var table = document.getElementById('form_result');    
-         //Add the data rows.
-            row = table.insertRow(-1);
-
-            var cell1 = row.insertCell(-1);
-            var cell2 = row.insertCell(-1);
-            var cell3 = row.insertCell(-1);
-            var cell4 = row.insertCell(-1);
-            var cell5 = row.insertCell(-1);
-            var cell6 = row.insertCell(-1);
-            var cell7 = row.insertCell(-1);
-            var cell8 = row.insertCell(-1);
-            var cell9 = row.insertCell(-1);
-            
-
-            cell1.innerHTML = data.name;
-            cell2.innerHTML = data.gender
-            cell3.innerHTML = data.dob;
-            cell4.innerHTML = data.nationality;
-            cell5.innerHTML = data.lang;
-            cell6.innerHTML = data.email;
-            cell7.innerHTML = data.degree;
-            cell8.innerHTML = data.add;
-           
-            cell9.innerHTML = `<a input type="button" onClick="onEdit(this)" style="color:blue;">Edit</a>
-                               <a input type="button" onClick="onDelete(this)" style="color:red";">Delete</a>`;
-             //here i am adding the row
-            table.appendChild(row);
-
-
-    }
-  
     function onEdit(td) {
     if (confirm('Make sure the updated details are entered in this form before clicking ok')) {
         row = td.parentElement.parentElement;
